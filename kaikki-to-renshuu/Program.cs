6fb6c7f153436d815ff43a1fc7971977065f5603
Wayne Bowie <wayneb@pcius.com>
2025-02-12 13:15:58 -0600

    Handle more edge cases

--- kaikki-to-renshuu/Program.cs
+++ kaikki-to-renshuu/Program.cs
@@ -25,7 +25,7 @@ if(string.IsNullOrWhiteSpace(exportPath))
 
 List<RenshuuRoot> conversion = [];
 
-foreach(var line in File.ReadAllLines(path))
+foreach (var line in File.ReadAllLines(path))
 {
     var word = JsonConvert.DeserializeObject<KaikkiRoot>(line);
     if(word == null)
@@ -40,13 +40,31 @@ foreach(var line in File.ReadAllLines(path))
 
     foreach (var form in word.forms)
     {
+        (string kana, string kanji) = (form.form, word.word);
+        var kanjiChars = GetCharsInRange(form.form, 0x4E00, 0x9FBF);
+        //var romajiInForm = GetCharsInRange(form.form, 0x0020, 0x007E);
+        //var romajiInWord = GetCharsInRange(word.word, 0x0020, 0x007E);
+        //var hiraganaChars = GetCharsInRange(form.form, 0x3040, 0x309F);
+        //var katakanaChars = GetCharsInRange(form.form, 0x30A0, 0x30FF);
+        if (kanjiChars.Count() > 0)
+        {
+            kanji = form.form;
+            kana = word.word;
+        }
+
         conversion.Add(new RenshuuRoot
         {
-            Kana = form.form,
-            Kanji = word.word,
-            Senses = word.senses.SelectMany(s => s.glosses).Where(e => e.Contains(form.form)).ToList()
+            Kana = kana,
+            Kanji = kanji,
+            Senses = word.senses.SelectMany(s => s.glosses).Where(e => e.Contains(kana)).ToList()
         });
     }
 }
 
 File.WriteAllText(exportPath, JsonConvert.SerializeObject(conversion, Formatting.Indented));
+
+// https://stackoverflow.com/questions/15805859/detect-japanese-character-input-and-romajis-ascii
+IEnumerable<char> GetCharsInRange(string text, int min, int max)
+{
+    return text.Where(e => e >= min && e <= max);
+}
